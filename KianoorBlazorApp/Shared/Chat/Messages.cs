31d0a3ff6e11b8b2e087e871ffa7363322a1f0e7
Ali Kianoor <askianoor@gmail.com>
2020-07-02 19:50:42 +0430

    Add project files.

---
+++ KianoorBlazorApp/Shared/Chat/Messages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KianoorBlazorApp.Shared.Chat
+{
+    public static class Messages
+    {
+        /// <summary>
+        /// Event name when a message is received
+        /// </summary>
+        public const string RECEIVE = "ReceiveMessage";
+
+        /// <summary>
+        /// Name of the Hub method to register a new user
+        /// </summary>
+        public const string REGISTER = "Register";
+
+        /// <summary>
+        /// Name of the Hub method to send a message
+        /// </summary>
+        public const string SEND = "SendMessage";
+
+    }
+}
