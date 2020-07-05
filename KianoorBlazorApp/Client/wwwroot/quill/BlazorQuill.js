6f15378d8d92909247b3efcd32ef47c2c0c3e510
Ali Kianoor <askianoor@gmail.com>
2020-07-06 00:27:44 +0430

    Add Emoji Support to the Preimum Chat

--- KianoorBlazorApp/Client/wwwroot/quill/BlazorQuill.js
+++ KianoorBlazorApp/Client/wwwroot/quill/BlazorQuill.js
@@ -1,14 +1,59 @@
 ﻿(function () {
     window.QuillFunctions = {
         createQuill: function (quillElement) {
+
+
+            //var toolbarOptions = {
+            //    container: [
+            //        ['bold', 'italic', 'underline', 'strike'],
+            //        ['blockquote', 'code-block'],
+            //        [{ 'header': 1 }, { 'header': 2 }],
+            //        [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+            //        [{ 'script': 'sub' }, { 'script': 'super' }],
+            //        [{ 'indent': '-1' }, { 'indent': '+1' }],
+            //        [{ 'direction': 'rtl' }],
+            //        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+            //        [{ 'color': [] }, { 'background': [] }],
+            //        [{ 'font': [] }],
+            //        [{ 'align': [] }],
+            //        ['clean'],
+            //        ['emoji'],
+            //        ['link']
+            //    ],
+            //    handlers: {
+            //        'emoji': function () { }
+            //    }
+            //}
+
+            var toolbarOptions = {
+                container: '#toolbar',
+                handlers: {
+                    'emoji': function () { }
+                }
+            }
+
             var options = {
                 modules: {
-                    toolbar: '#toolbar'
+                    "toolbar": toolbarOptions,
+                    "emoji-toolbar": true,
+                    "emoji-shortname": true,
+                    "emoji-textarea": true
                 },
                 placeholder: 'Compose an epic...',
                 readOnly: false,
                 theme: 'snow'
             };
+
+            //var options = {
+            //    modules: {
+            //        toolbar: '#toolbar',
+            //    },
+            //    placeholder: 'Compose an epic...',
+            //    readOnly: false,
+            //    theme: 'snow'
+            //};
+
+
             // set quill at the object we can call
             // methods on later
             new Quill(quillElement, options);
